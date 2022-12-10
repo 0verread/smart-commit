d940c55ae97be42e646685bb76d41c36cff49eba
sub0ne1 <iam0verread@gmail.com>
2022-12-09 22:16:40 -0500

    Almost Done

--- src/smartcommit.py
+++ src/smartcommit.py
@@ -1,16 +1,16 @@
-import openai_api
+import openai
 import os
 import subprocess
 
-api_token = os.environ.get("OPENAI_API_KEY")
-if api_token is None:
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+if openai.api_key is None:
   print("Please set the OPENAI_API_KEY environment variable.")
-
+  
 
 git_staged_cmd = subprocess.run(["git", "diff", "--staged"], stdout=subprocess.PIPE)
 git_staged_cmd = git_staged_cmd.stdout.decode("utf-8")
 
-if git_staged_cmd.len() == 0:
+if len(git_staged_cmd) == 0:
   print("There are no staged files to commit.\nTry running git add to stage some files.")
 
 is_repo = subprocess.run(["git", "rev-parse", "--is-inside-work-tree"], stdout=subprocess.PIPE)
@@ -19,14 +19,20 @@ is_repo = is_repo.stdout.decode("utf-8").strip()
 if is_repo != "true":
   print("It looks like you are not in a git repository.\nPlease run this command from the root of a git repository, or initialize one using git init.")
 
-client = openai_api.Client.new(api_token)
+# client = openai.Client.new(api_token)
 
 output = subprocess.run(["git", "diff", "HEAD"], stdout=subprocess.PIPE)
 output = output.stdout.decode("utf-8")
+# print(output)
 
-prompt_args = openai_api.api.CompletionArgs.builder().prompt(f"git diff HEAD\n{output}\n\n# Write a commit message describing the changes and the reasoning behind them\ngit commit -F- <<EOF").engine("code-davinci-002").temperature(0.0).max_tokens(2000).stop(["EOF"])
+# prompt_args = openai.CompletionArgs.builder().prompt(f"git diff HEAD\n{output}\n\n# Write a commit message describing the changes and the reasoning behind them\ngit commit -F- <<EOF").engine("code-davinci-002").temperature(0.0).max_tokens(2000).stop(["EOF"])
 
-prompt_response = client.completions.create(prompt_args.build())
+prompt_response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=f"git diff HEAD\n{output}\n\n# Write a commit message describing the changes and the reasoning behind them\ngit commit -F- <<EOF",
+    max_tokens=1024,
+    temperature=0.5
+)
 
 commit_message = prompt_response.get_completions()[0]["text"].strip()
 
