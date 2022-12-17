deed7ae1ad1d0a1186e7e7081073cca6b64e03ea
sub0ne1 <iam0verread@gmail.com>
2022-12-16 22:00:33 -0500

    Changes and clean up

--- src/smartcommit.py
+++ src/smartcommit.py
@@ -19,23 +19,21 @@ is_repo = is_repo.stdout.decode("utf-8").strip()
 if is_repo != "true":
   print("It looks like you are not in a git repository.\nPlease run this command from the root of a git repository, or initialize one using git init.")
 
-# client = openai.Client.new(api_token)
-
 output = subprocess.run(["git", "diff", "HEAD"], stdout=subprocess.PIPE)
 output = output.stdout.decode("utf-8")
-# print(output)
 
 # prompt_args = openai.CompletionArgs.builder().prompt(f"git diff HEAD\n{output}\n\n# Write a commit message describing the changes and the reasoning behind them\ngit commit -F- <<EOF").engine("code-davinci-002").temperature(0.0).max_tokens(2000).stop(["EOF"])
 
 prompt_response = openai.Completion.create(
     model="text-davinci-002",
-    prompt=f"git diff HEAD\n{output}\n\n# Write a commit message describing the changes and the reasoning behind them\ngit commit -F- <<EOF",
+    prompt=f"git diff HEAD\n{output}\n\n# Write one commit message describing the changes and the reasoning behind them\ngit commit -F- <<EOF",
     max_tokens=1024,
     temperature=0.5
 )
+commit_messages = prompt_response.choices[0]["text"]
+# print(commit_messages)
+# commit_message = prompt_response.get_completions()[0]["text"].strip()
 
-commit_message = prompt_response.get_completions()[0]["text"].strip()
+subprocess.run(["git", "commit", "-m-", "<<EOF"], input=commit_messages.encode("utf-8"))
 
-subprocess.run(["git", "commit", "-F-", "<<EOF"], input=commit_message.encode("utf-8"))
-
-print(f"Commit message generated: {commit_message}")
+print(f"Commit message generated: {commit_messages}")
