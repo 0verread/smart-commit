98c6c07dc6c54c2f7afcbc6718d139e63624574f
sub0ne1 <iam0verread@gmail.com>
2022-12-24 18:33:33 -0500

    few more changes

--- src/smartcommit.py
+++ src/smartcommit.py
@@ -1,46 +1,82 @@
+#!/usr/bin/env python
+
+# pyInquirer
+# Typer/click
+# curses
+
 import openai
 import os
 import subprocess
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+import typer
+from rich.progress import Progress, SpinnerColumn, TextColumn
+from InquirerPy import prompt
+
+is_commitmsg_ready = False
+
+prompt_questions = [
+  {
+    'type': 'list',
+    'name': 'options',
+    'message': 'Commit message is ready. what do you want to do?',
+    'choices': [
+      'Commit',
+      'See Commit message',
+      'Edit Commit Message'
+    ]
+  }
+]
 
 def check_api_key():
   openai.api_key = os.environ.get("OPENAI_API_KEY")
   if openai.api_key is None:
     print("Please set the OPENAI_API_KEY environment variable.")
     return
+  return openai.api_key
 
-check_api_key()
+def get_commit_msg():
+  git_staged_cmd = subprocess.run(["git", "diff", "--staged"], stdout=subprocess.PIPE).stdout.decode("utf-8")
 
-git_staged_cmd = subprocess.run(["git", "diff", "--staged"], stdout=subprocess.PIPE)
-git_staged_cmd = git_staged_cmd.stdout.decode("utf-8")
+  if len(git_staged_cmd) == 0:
+    print("There are no staged files to commit.\nTry running git add to stage some files.")
+    return
+  is_repo = subprocess.run(["git", "rev-parse", "--is-inside-work-tree"], stdout=subprocess.PIPE)
+  is_repo = is_repo.stdout.decode("utf-8").strip()
 
-if len(git_staged_cmd) == 0:
-  print("There are no staged files to commit.\nTry running git add to stage some files.")
-
-is_repo = subprocess.run(["git", "rev-parse", "--is-inside-work-tree"], stdout=subprocess.PIPE)
-is_repo = is_repo.stdout.decode("utf-8").strip()
-
-if is_repo != "true":
-  print("It looks like you are not in a git repository.\nPlease run this command from the root of a git repository, or initialize one using git init.")
-
-output = subprocess.run(["git", "diff", "HEAD"], stdout=subprocess.PIPE)
-output = output.stdout.decode("utf-8")
-
-prompt_response = openai.Completion.create(
+  if is_repo != "true":
+    print("It looks like you are not in a git repository.\nMake sure you are in a git repository.")
+    return
+  output = subprocess.run(["git", "diff", "HEAD"], stdout=subprocess.PIPE).stdout.decode("utf-8")
+  prompt_response = openai.Completion.create(
     model="text-davinci-002",
-    prompt=f"git diff HEAD\n{output}\n\n# Please generate a commit message describing the changes",
+    prompt=f"git diff HEAD\n{output}\n\n#Write a one line git commit message that describes the changes",
     max_tokens=1024,
     temperature=0.5
-)
-commit_messages = prompt_response.choices[0]["text"]
+  )
+  commit_messages = prompt_response.choices[0]["text"] 
+  is_commitmsg_ready=True
+  return commit_messages
 
-subprocess.run(["git", "commit", "-m", commit_messages], input=commit_messages.encode("utf-8"))
+def commit_changes():
+  check_api_key()
+  commit_msg = get_commit_msg()
+  return commit_msg
 
-print(f"Commit message generated: {commit_messages}")
+  # subprocess.run(["git", "commit", "-m", commit_msg], input=commit_msg.encode("utf-8"))
 
-# generate_git_commit():
+def main():
+  with Progress(
+    SpinnerColumn(),
+    TextColumn("[progress.description]{task.description}"),
+  ) as progress:
+    task = progress.add_task(description="Generating Commit message...", total=None)
+    commit_msg  = commit_changes()
+    if is_commitmsg_ready:
+      progress.update(task, advance=1)
+  ans  = prompt(prompt_questions)
+  print(f"Commit message generated: {commit_msg}")
+  print("Done!")
 
 
-# # if __name__ = "__main__":
-# #   generate_git_commit
+if __name__ == "__main__":
+  main()
